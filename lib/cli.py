d51cbafde1fee9489fe2e1a176d729be9be999c1
Terry <terry.otieno@student.moringaschool.com>
2024-09-20 09:28:52 +0300

    added classes to cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,4 +1,3 @@
-
 from helpers import (
     function_1, function_2,
     function_3, function_4, 
@@ -8,37 +7,45 @@ from helpers import (
 )
 import debug
 
-def main():
-    print('Welcome to my CLI!') #greetings
-    debud.log_info('CLI started')
-    
-    function_1()
-    
-    x = 0
-    while not x: 
-        x = function_2(x)
-        debug.check_variable('x',x)
+class MyCLI:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+    def start(self):
+        print('Welcome to my CLI!')  # greetings
+        debug.log_info('CLI started')
         
-    if x < 0:
-        y = function_3(x) 
-        debug.check_variable('y',y) 
-    else:
-        y = function_4(x)  
-        debug.check_variable('y',y) 
+        function_1()
         
+        self.run_loop()
+        self.process_values()
         
-    z = function_5(y)  
-    debug.check_variable('z',z) 
-    z = function_6(z)
-    z = function_7(z)
-    z = function_8(z)
-    
-    
-    function_9(z)  
-    function_10(x,y,z)
-    
-    print('Thanks for using my CLI!')#closing message
-    
-    if __name__ == '__main__':
-        main()
-        
+        print('Thanks for using my CLI!')  # closing message
+
+    def run_loop(self):
+        while not self.x:
+            self.x = function_2(self.x)
+            debug.check_variable('x', self.x)
+
+    def process_values(self):
+        if self.x < 0:
+            self.y = function_3(self.x) 
+            debug.check_variable('y', self.y) 
+        else:
+            self.y = function_4(self.x)  
+            debug.check_variable('y', self.y) 
+
+        self.z = function_5(self.y)  
+        debug.check_variable('z', self.z) 
+        self.z = function_6(self.z)
+        self.z = function_7(self.z)
+        self.z = function_8(self.z)
+
+        function_9(self.z)  
+        function_10(self.x, self.y, self.z)
+
+if __name__ == '__main__':
+    cli = MyCLI()
+    cli.start()
